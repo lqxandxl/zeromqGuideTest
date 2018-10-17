6a1f5341b490b894caa283124620ad226df1fc0b
liqixin <buptliqixin@163.com>
2018-10-17 15:32:32 +0800

    fix why send a string  bug receive more charactor

--- client1/client.cpp
+++ client1/client.cpp
@@ -6,6 +6,17 @@
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string int_to_string(int x){
+    stringstream ss;
+    ss<<x;
+    string res;
+    ss>>res;
+    return res;
+}
 
 int main ()
 {
@@ -18,15 +29,33 @@ int main ()
 
     //  Do 10 requests, waiting each time for a response
     for (int request_nbr = 0; request_nbr != 1000; request_nbr++) {
-        zmq::message_t request (5);
-        memcpy (request.data (), "Hello", 5); //内容拷贝到request中
-        std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
+        string req;
+        req = "Hello";
+        string numS = int_to_string(request_nbr);
+        req = req+numS;
+        req=req;      
+        int reqSize = req.size();
+        zmq::message_t request (reqSize+1); //这里必须+1留一个赋予给\0，string结尾是否有\0不同编译器貌似不一样，留一个位置就没bug了。
+        memcpy (request.data (), req.c_str(), reqSize+1); //内容拷贝到request中
+        string res = static_cast<char *>(request.data());
+        std::cout << "Sending Hello " << req << "and req size is "<< req.size() << std::endl;
         socket.send (request); //利用socket发送
 
         //  Get the reply.
         zmq::message_t reply;
         socket.recv (&reply);  //接受消息，具体哪个端口接受消息？没说
-        std::cout << "Received World " << request_nbr << std::endl;
+        string res2 ((char *)reply.data());
+        for(int i=0;i<(int)res2.size();++i){
+            if(res2[i]=='\n'){
+                cout<<"have another line mark"<<endl;
+            }
+            if(res2[i]==' '){ //除了客户端发送内容外，后面还跟着两个未知的字符，然后又跟了一个socket-type的内容。过一段时间未知字符消失。
+                cout<<"have blank in word"<<endl;
+            }
+            cout<<res2[i]<<endl;
+            cout<<"i is "<<i<<endl;
+        }  
+        std::cout << "Received World " << request_nbr<<"reply from server is "<< res2 << std::endl;
     }
     return 0;
 }
--- server1/server.cpp
+++ server1/server.cpp
@@ -14,25 +14,40 @@
 #define sleep(n)    Sleep(n)
 #endif
 
+using namespace std;
+
 int main () {
     //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
-
+    int count=0;
     while (true) {
         zmq::message_t request;
 
         //  Wait for next request from client
         socket.recv (&request); //阻塞在这个地方，等待消息的到来
-        std::cout << "Received Hello" << std::endl;
-
+        string res ((char *)request.data());
+        for(int i=0;i<(int)res.size();++i){
+            if(res[i]=='\n'){
+                cout<<"have another line mark"<<endl;
+            }
+            if(res[i]==' '){ //除了客户端发送内容外，后面还跟着两个未知的字符，然后又跟了一个socket-type的内容。过一段时间未知字符消失。
+                cout<<"have blank in word"<<endl;
+            }
+            //之所以末尾有奇怪的东西，是因为发的时候长度不够，没有留下一个\0的位置。没有结尾，所以收到的会变长。
+            cout<<res[i]<<endl;
+            cout<<"i is "<<i<<endl;
+        }
+        std::cout << "Received |" << res <<"| size is "<<res.size()<< std::endl;
+        cout<<"have received count is"<<count<<endl;
+        count++;
         //  Do some 'work'
         sleep(1);
 
         //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy (reply.data (), "World", 5);
+        zmq::message_t reply (6);
+        memcpy (reply.data (), "World\0", 6);      
         socket.send (reply); //回响应，具体回哪个地址哪个端口也没说，反正就是回了
     }
     return 0;
